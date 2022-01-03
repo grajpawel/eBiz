ec94848f8d85d38341b01633e0623c1e5d01aac0
wercabaj <wercabaj@gmail.com>
2022-01-03 15:10:22 +0100

    New changes to stage 2

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,13 @@
 FROM prestashop/prestashop:1.7.8.0-7.1-apache
 
+RUN rm -rf *
+
+COPY shop-data/ ./
+
+RUN chown -R www-data:root ./
+
+COPY ssl/ /etc/apache2/sites-available
+
 RUN a2enmod ssl
 
 EXPOSE 80
--- shop-data/modules/ps_eventbus/translations/pl.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-global $_MODULE;
-$_MODULE = array();
-$_MODULE['<{ps_eventbus}prestashop>ps_eventbus_ec6d9b006b92fe20e87ff8d96b018c66'] = 'PrestaShop Eventbus';
-$_MODULE['<{ps_eventbus}prestashop>ps_eventbus_75542b69a465397b8d8667bf3783a535'] = 'Połącz swoje konto PrestaShop, aby zsynchronizować dane swojego sklepu z wybranymi przez Ciebie partnerami. Nie odinstaluj tego modułu, jeśli już korzystasz z usługi, ponieważ uniemożliwi to jej działanie.';
-$_MODULE['<{ps_eventbus}prestashop>ps_eventbus_d93b27a5e3149cbc4033dae34a509b01'] = 'To działanie uniemożliwi natychmiastowe działanie usług PrestaShop i usług społecznościowych, ponieważ używają one modułu PrestaShop Eventbus do synchronizacji.';
