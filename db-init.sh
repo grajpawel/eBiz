facafaf3fe173e17aae3ba61bef6bf9b638cd400
wercabaj <wercabaj@gmail.com>
2022-01-06 13:20:33 +0100

    Stage 2 configuration changes

---
+++ db-init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CONTAINER="admin-mysql_db.1.jhmt04hxq732lvnr76m7ikzlv"
+
+docker cp ./*.sql $CONTAINER:/tmp/BE_180061.sql
+docker cp ./script.sh $CONTAINER:/tmp/script.sh
+docker exec -it $CONTAINER  chmod 777 /tmp/script.sh
+docker exec -it $CONTAINER  chmod 777 /tmp/BE_180061.sql
+docker exec -it $CONTAINER /bin/sh /tmp/script.sh
+docker exec -it $CONTAINER  rm /tmp/BE_180061.sql
+docker exec -it $CONTAINER  rm /tmp/script.sh
+echo "KONIEC"
+
